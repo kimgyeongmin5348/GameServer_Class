db9b7ff8041537b83c51b2e9e030269123bea9aa
kimgyeongmin5348 <135432080+kimgyeongmin5348@users.noreply.github.com>
2025-03-13 10:24:57 +0900

    1주차_2 정리본들

--- Gameserver_two/with_professer_1week.cpp
+++ Gameserver_two/with_professer_1week.cpp
@@ -1,1 +1,30 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
+using namespace std;
+using namespace chrono;
+
+int CACHE_LINE_SIZE = 32;
+
+int main()
+{
+	for (int i = 0; i < 20; ++i) {
+		const int size = 1024 << i;
+		long long* a = (long long*)malloc(size);
+		unsigned int index = 0;
+		long long tmp = 0;
+		const int num_data = size / 8;
+		auto start = high_resolution_clock::now();
+		for (int j = 0; j < 100000000; ++j) {
+			tmp += a[index % size];
+			index += CACHE_LINE_SIZE * 11;
+		}
+
+
+		auto duration = high_resolution_clock::now() - start;
+		cout << "Size : " << size / 1024 << " K, ";
+		cout << "Time " << duration_cast <milliseconds>(duration).count();
+		cout << " msec\n";
+		cout << "RESULT " << tmp << endl;
+	}
+}
---
+++ Gameserver_two/with_professer_2week.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+using namespace std;
+
+int main()
+{
+
+}
